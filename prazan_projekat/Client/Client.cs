0b28e07fa21fcddfd0c8a91c8786bc571e833375
milos susic <milossusic22@gmail.com>
2024-12-28 09:11:57 +0100

    promjene

---
+++ prazan_projekat/Client/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace Client
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+
+            #region Povezivanje
+
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 50001);
+            byte[] buffer = new byte[1024];
+
+
+            clientSocket.Connect(serverEP);
+            Console.WriteLine("Klijent je uspesno povezan sa serverom!");
+            #endregion
+
+            while (true)
+            {
+                Console.WriteLine("Unesite id korisnika");
+                string id = Console.ReadLine();
+                Console.WriteLine("Unesite ime korisnika");
+                string ime = Console.ReadLine();
+                Console.WriteLine("Unesite prezime korisnika");
+                string prezime = Console.ReadLine();
+
+
+              
+
+             
+
+            }
+        }
+    }
+}
--- prazan_projekat/Server/Server.cs
+++ prazan_projekat/Server/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Server
 {
@@ -11,6 +12,16 @@ namespace Server
 
         static void Main(string[] args)
         {
+            byte[] buffer = new byte[4096];
+
+            List<Korisnik> rezultati = new List<Korisnik>()
+            {
+                new Korisnik("123747","Milos","Susic",0),
+                new Korisnik("214532","Bozana","Todorovic",0)
+            };
+
+
+
         }
     }
 }
